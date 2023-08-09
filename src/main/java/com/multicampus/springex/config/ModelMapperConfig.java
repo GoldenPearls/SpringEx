b5cf1bf605c64b52a9f4bb8eadf1a762481c38a5
[GoldenPearls] <[prettylee620@naver.com]>
2023-08-10 02:08:45 +0900

    8월 9일 수업 내용 공부한 내용 추가 주석 달기 + 공부 내용 1. root-context.xml mybatis:scans는 패키지를 스캔하도록 설정
    2. TodoMapper.xml과 TodoMapper.java로 쓰는 이유
    - 어노테이션 TodoMapper.java.에 한번에 @Select("select * form tb1_board") 이렇게 써도 되는데 코드 수정하고 다시 빌드 하는 등의 유지 보수성이 떨어져서 따로 분리작성
    
    3. TodoMapper.xml은 정적인 resources 안에 넣어줘야 함
    
    4. ModelMapperConfig.java에 대한 주석 추가
    - setFieldMatchingEnabled(true)
    - setFieldAccessLevel(...)
    - setMatchingStrategy(...)
    - ModelMapper에 대한 내용 공부
    
    5. TodoServiceImpl.java 주석

--- src/main/java/com/multicampus/springex/config/ModelMapperConfig.java
+++ src/main/java/com/multicampus/springex/config/ModelMapperConfig.java
@@ -11,7 +11,13 @@ public class ModelMapperConfig {
     @Bean //bean 등록
     public ModelMapper getMapper(){
         //VO를 받아오는 코드
+        // ModelMapper 라이브러리를 이용해서 객체 간의 매핑을 수행하는 설정 변경
+        // ModelMapper는 객체 간의 속성값을 자동으로 매핑해주는 라이브러리로 객체 간의 변환 작업 편리
         ModelMapper modelMapper = new ModelMapper();
+        // setFieldMatchingEnabled(true) : 필드 기반 설정, 객체의 필드들 간의 이름이 같은 경우 자동 매칭
+        // setFieldAccessLevel : 매핑할 필드의 접근 레벨 지정하며, PRIVATE 레벨로 설정되어 있는 경우 private 접근 제어자로
+        // 선언된 필드도 매핑 대상에 포함
+        // setMatchingStrategy은 매칭 전략을 지정하면 STRICT 전략시, 매핑되지 않는 속성이 있는 경우 예외 발생하며 매핑 작업 실패
         modelMapper.getConfiguration().setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE) //setFieldAccessLevel : 레벨을 정함
                 .setMatchingStrategy(MatchingStrategies.STRICT);
--- src/main/java/com/multicampus/springex/service/TodoServiceImpl.java
+++ src/main/java/com/multicampus/springex/service/TodoServiceImpl.java
@@ -16,12 +16,16 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor //싱글톤으로 지정한 빈을 생성자 주입에 의해 injection
 public class TodoServiceImpl implements TodoService{
     //의존성 주입이 필요한 객체타입을 final로 고정하고 생성자를 생성하여 주입
+    // 해당 클래스의 의존성이며, final로 선언되어 변경되지 않음을 보장
     private final TodoMapper todoMapper;
     private final ModelMapper modelMapper;
 
+    // todo 항목등록하는 메서드
     @Override
     public void register(TodoDTO todoDTO) {
         log.info(modelMapper);
+        // modelMapper을 사용하여 TodoDTO 객체를 TodoVO객체로 변환하고
+        // 이를 TodoMapper를 통해 데이터베이스 등록
         TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
         log.info(todoVO);
         //TodoMapper가 받음 => TodoMapper.xml이 들어가서 동작
@@ -29,10 +33,13 @@ public class TodoServiceImpl implements TodoService{
     }
 
     // 구현 객체이기 때문에 TodoService가 바뀌면 바뀜
+    // 모든 할 일 항목 조회
     @Override
     public List<TodoDTO> getAll() {
 
-        //stream은 vo는 map을 통해서 TodoDTO로 바꾸고 Collectors,toList()로 바뀐 TodoDTO를 묶어줌
+        // 데이터베이스에서 조회한 할일 항목을 VO에서 DTO로 변환하여 리스트로 반환하는 작업을 수행
+        // todoMapper를 통해 데이터 베이스에서 모든 할일 조회 => stream 변환 => TodoDTO 변환
+        // VO객체를 DTO 객체로 변환하여 매핑 => 리스트 수집후 생성
         List<TodoDTO> dtoList = todoMapper.selectAll().stream()
                 .map(vo-> modelMapper.map(vo, TodoDTO.class))
                 .collect(Collectors.toList());
