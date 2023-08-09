b567839b23425b0b62df4c3fb37bae156bb6a312
[GoldenPearls] <[prettylee620@naver.com]>
2023-08-09 15:18:57 +0900

    8월 9일 수업 내용 7 TodoController의 Get/Post 방식 처리 1.register.jsp - register 화면 작성
    2. TodoController.java 수정
    - TodoController에 model를 넣어줌

--- src/main/java/com/multicampus/springex/controller/TodoController.java
+++ src/main/java/com/multicampus/springex/controller/TodoController.java
@@ -4,31 +4,35 @@ import com.multicampus.springex.dto.TodoDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller //스프링 MVC에서 컨트롤러 역할, 스프링의 빈(Bean)으로 등록
 @RequestMapping("/todo")
 @Log4j2
 public class TodoController {
     @RequestMapping("/list") //localhost:8090/todo/list
-    public void list(){
-        log.info("todo_lis.....");
+    public void list(Model model){
+        log.info("todo_list");
     }
 
     //@RequestMapping(value="/register", method = RequestMethod.GET) //localhost:8090/todo/register
     @GetMapping("/register")
     /*제대로 불러오는지 확인하는 작업, 제대로 불러오면 콘솔에 뜬다. */
-    public void register(){
+    public void registerGET(){
         log.info("GET todo_register.....");
     }
 
     // register은 post와 get방식 따로 따로, 객체를 알아서 넣어줌
     @PostMapping("/register")
-    public void registerPost(TodoDTO todoDTO){
+    public String registerPost(TodoDTO todoDTO, RedirectAttributes redirectAttributes){
         log.info("Post todo register");
         log.info(todoDTO);
+
+       return "redirect:/todo/list";
     }
 }
